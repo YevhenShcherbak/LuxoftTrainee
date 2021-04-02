6c257261a65b95ad6ad737c8bab874b83702812b
Yevhen Shcherbak <sherbak13579@gmail.com>
2021-04-03 02:01:06 +0300

    add pop_back/pop_forward and showList

--- First task/ListRealis.cpp
+++ First task/ListRealis.cpp
@@ -3,6 +3,21 @@
 template<typename Item>
 class List
 {
+	template<typename Item>
+	class Node
+	{
+	public:
+		Node<Item>* pNext;
+		Item value;
+
+		Node(Item value = Item(), Node<Item>* pNext = nullptr)
+		{
+			this->value = value;
+			this->pNext = pNext;
+		}
+	};
+	Node<Item>* head;
+	int size;
 public:
 	List();
 	~List();
@@ -13,23 +28,6 @@ public:
 	void pop_forward();
 	void pop_back();
 	void removeAt(const int index);
-private:
-	template<typename Item>
-	class Node
-	{
-	public:
-		Node<Item>* pNext;
-		Item value;
-		
-		Node(Item value = Item(), Node<Item>* pNext = nullptr)
-		{
-			this->value = value;
-			this->pNext = pNext;
-		}
-	};
-	Node<Item>* head;
-	int size;
-
 };
 
 template<typename Item>
@@ -87,6 +85,51 @@ void List<Item>::push_forward(Item value)
 	++size;
 }
 
+template<typename Item>
+void List<Item>::pop_forward()
+{
+	if (GetSize() == 0)
+	{
+		std::cout << "List is empty" << std::endl;
+	}
+	else 
+	{
+		Node<Item>* temp = head;
+		head = head->pNext;
+		delete temp;
+		--size;
+	}
+}
+
+template<typename Item>
+void List<Item>::pop_back()
+{
+	if (GetSize() == 0)
+	{
+		std::cout << "List is empty" << std::endl;
+	}
+	else 
+	{
+		Node<Item>* current = this->head;
+		while (current != nullptr)
+		{
+			current = current->pNext;
+		}
+		delete current;
+		
+		--size;
+	}
+}
+
+template<typename Item>
+void showList(List<Item> list) {
+	for (int i = 0; i < list.GetSize(); i++)
+	{
+		std::cout << list[i] << ' ';
+	}
+	std::cout << std::endl;
+}
+
 int main()
 {
 	List<int> list1;
@@ -94,12 +137,17 @@ int main()
 	list1.push_back(10);
 	list1.push_back(11);
 	list1.push_forward(4);
-	
-	for (int i = 0; i < list1.GetSize(); i++)
-	{
-		std::cout << list1[i] << std::endl;
-	}
-	
+	showList(list1);
+	//std::cout << list1.GetSize() << std::endl;
+
+	list1.pop_back();
+	list1.pop_back();
+	showList(list1);
+	list1.pop_back();
+	list1.pop_back();
+	list1.pop_back();
+	showList(list1);
+	//std::cout << list1.GetSize() << std::endl;
 
 	return 0;
 }
